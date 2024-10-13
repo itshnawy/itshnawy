1cdb9f524c0734d4dc2ba47d42c227997418a388
Mohammed L Shnawy <elshenawy.mohammed7@gmail.com>
2024-10-13 17:47:59 +0300

    EXERCISE #1: Flashcards

---
+++ practicing/Maxmilian-Courses/Master-React/flashcards/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./app.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FlashCards />
+    </div>
+  );
+}
+
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript"
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components"
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX"
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props"
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook"
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element"
+  }
+];
+
+function FlashCards() {
+   let [isClickedId,setIsClickedId] = useState(null)
+
+function handleClick(id) {
+  setIsClickedId(id !== isClickedId ? id : null)
+}
+  return <div className="flashcards">
+    {questions.map(questions =>
+    <div onClick={() => handleClick(questions.id)} key={questions.id} style={questions.id === isClickedId ? {background:"#000",color:"#fff"} : {background:"#f7f7f7"} }>
+      <p>{questions.id === isClickedId ? questions.answer : questions.question}</p></div>)}
+  </div>;
+}
