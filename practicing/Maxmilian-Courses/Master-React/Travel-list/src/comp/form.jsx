721879b5d9f035461d459b6d94da843db55e243d
Mohammed L Shnawy <elshenawy.mohammed7@gmail.com>
2024-10-13 17:12:33 +0300

    controled elements

--- practicing/Maxmilian-Courses/Master-React/Travel-list/src/comp/form.jsx
+++ practicing/Maxmilian-Courses/Master-React/Travel-list/src/comp/form.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 function Form () {
+
+    const [name,setName] = useState('');
+    const [quantity,setQuantity] = useState(1);
+
     function handleSubmit(e) {
     e.preventDefault();
+    if (!name) return;
+
+    const newItem = {
+        id: Date.now(),
+        name,
+        quantity,
+        packed: false
+    }
+    console.log(newItem)
+    setName('');
+    setQuantity(1)
     }
     return <>
     <div id="form">
     <h3>What do you need for your trip?</h3>
     <form onSubmit={(e) => handleSubmit(e)} >
-    <input type="number" placeholder="1" min="1" max="99"/>
-    <input type="text" placeholder="item" /><br />
+    <input type="number" placeholder="1" min="1" max="99" value={quantity} onChange={(e) => setQuantity(parseInt(e.target.value))}/>
+    <input type="text" placeholder="item" value={name} onChange={(e) => setName(e.target.value)}/><br />
     <button>Add Item</button>
     </form>
     </div>
